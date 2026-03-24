1ef278a0364c73b75c765f486fd87b7abfea2169
Kinga Lepiarz <lepiarzkinga@gmail.com>
2026-03-24 19:10:04 +0100

    lab4

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,66 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  List<Task> tasks = [
+    Task(title: "Labolatorium 4", deadline: "jutro"),
+    Task(title: "Projekt z metodologi", deadline: "dzisiaj"),
+    Task(title: "Przeczytać książkę", deadline: "w tym tygodniu"),
+    Task(title: "Zrobić zakupy", deadline: "w tym tygodniu")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        child: Center(
+          children: [
+            Text("Dzisiejsze zadania"),
+            style: TextStyle(
+              fontSize:21,
+              fontWeight.bold,
+            ),
+          ]
         appBar: AppBar(
             title: Text("KrakFlow")),
-        body: Center(
-          child: Column(
-            children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
-            ],
-          ),
+
+        body: ListView.builder(
+          itemCount: tasks.length,
+          itemBuilder: (context, index) {
+            return Text(tasks[index].title);
+          },
         ),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String title;
+  final String deadline;
+
+  Task({required this.title, required this.deadline});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
       ),
     );
   }
